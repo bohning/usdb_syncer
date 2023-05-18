7d730e086870f994831956bf5efe8a3aae1ea956
mjhalwa <mjhalwa.codes@gmail.com>
2023-05-18 21:54:10 +0200

    fixes song staging error for row = 0

--- src/usdb_syncer/gui/song_table/song_table.py
+++ src/usdb_syncer/gui/song_table/song_table.py
@@ -303,7 +303,9 @@ class SongTable:
         _logger.info(f"Added {len(rows)} songs to batch.")
 
     def stage_song_ids(self, song_ids: list[SongId]) -> None:
-        self._stage_rows(row for id in song_ids if (row := self._model.row_for_id(id)))
+        self._stage_rows(
+            row for id in song_ids if (row := self._model.row_for_id(id)) is not None
+        )
 
     def set_selection_to_song_ids(self, select_song_ids: list[SongId]) -> None:
         select_indices = self._model.indices_for_ids(select_song_ids)
