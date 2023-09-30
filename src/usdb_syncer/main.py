a90b794a2bec64c9ada017496e90733858ad9a6f
RumovZ <gp5glkw78@relay.firefox.com>
2023-09-30 12:07:17 +0200

    Implement profiling

--- src/usdb_syncer/main.py
+++ src/usdb_syncer/main.py
@@ -1,34 +1,64 @@
 """Run the app."""
 
 import argparse
+import cProfile
 import logging
+import subprocess
 import sys
 import traceback
 from types import TracebackType
-from typing import Any
+from typing import Any, Callable
 
 from tools import generate_pyside_files
 from usdb_syncer.utils import AppPaths
 
 
-def excepthook(
+class Args:
+    """Command line args for the USDB Syncer."""
+
+    profile = False
+
+
+def _excepthook(
     error_type: type[BaseException], error: BaseException, tb_type: TracebackType | None
 ) -> Any:
     text = "    ".join(traceback.format_exception(error_type, error, tb_type)).strip()
     logging.error(f"Uncaught exception:\n    {text}")
 
 
+def _parse_args() -> Args:
+    parser = argparse.ArgumentParser(
+        description="An app to download and synchronize UltraStar songs."
+    )
+    parser.add_argument(
+        "-p", "--profile", action="store_true", help="Write profiling data on exit."
+    )
+    return parser.parse_args(namespace=Args())
+
+
+def _with_profile(func: Callable[[], None]) -> None:
+    print("Running with profiling enabled.")
+    profiler = cProfile.Profile()
+    profiler.enable()
+    func()
+    profiler.disable()
+    profiler.dump_stats(AppPaths.profile)
+    subprocess.call(["snakeviz", AppPaths.profile])
+
+
 def cli_entry(run_tools: bool = True) -> None:
-    sys.excepthook = excepthook
+    sys.excepthook = _excepthook
+    args = _parse_args()
     AppPaths.make_dirs()
-    parser = argparse.ArgumentParser(description="UltraStar script.")
-    _args = parser.parse_args()
     if run_tools:
         generate_pyside_files()
 
     from usdb_syncer.gui.mw import main  # pylint: disable=import-outside-toplevel
 
-    main()
+    if args.profile:
+        _with_profile(main)
+    else:
+        main()
 
 
 if __name__ == "__main__":
