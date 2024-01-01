b995585dd65f4d5e4d59e2d7a399e78713915563
mjhalwa <mjhalwa.codes@gmail.com>
2024-01-02 00:16:14 +0100

    removes base64 encoded cover image from json export (exports too huge)

--- src/usdb_syncer/json.py
+++ src/usdb_syncer/json.py
@@ -1,12 +1,8 @@
 """Generates a JSON from the passed song list."""
 
-# maybe reportlab is better suited?
-import base64
 import datetime
 import json
 from typing import Any, Iterable
-
-from PIL import Image
 
 from usdb_syncer import SongId
 from usdb_syncer.constants import VERSION
@@ -18,36 +14,6 @@ from usdb_syncer.usdb_scraper import UsdbSong
 _logger = get_logger(__file__)
 
 MAX_SIZE = (100, 100)
-
-
-def encode_image_to_json(
-    image_path: str, max_size: tuple[int, int] | None = None
-) -> str:
-    """encode it to Base64, and store it in a JSON string with optional resize"""
-    if max_size:
-        # Open the image using Pillow
-        with Image.open(image_path) as image:
-            # Resize the image
-            resized_image = image.resize(max_size)
-
-            # Convert resized image to bytes
-            with resized_image.convert("RGB") as resized_image_bytes:
-                # Encode image data to Base64
-                encoded_image = base64.b64encode(resized_image_bytes.tobytes()).decode(
-                    "utf-8"
-                )
-    else:
-        with open(image_path, "rb") as image_file:
-            # Encode image data to Base64
-            encoded_image = base64.b64encode(image_file.read()).decode("utf-8")
-
-    # Create a dictionary with the image data
-    image_data = {"image": encoded_image}
-
-    # Convert the dictionary to a JSON string
-    json_string = json.dumps(image_data)
-
-    return json_string
 
 
 def get_headers(txt_path: str) -> Headers:
@@ -75,13 +41,6 @@ def generate_song_json(songs: Iterable[SongId], path: str) -> int:
             ),
             "cover_meta": (
                 meta.meta_tags.cover.to_str("co") if meta.meta_tags.cover else None
-            ),
-            "cover_preview": (
-                encode_image_to_json(
-                    str(meta.path.with_name(meta.cover.fname)), MAX_SIZE
-                )
-                if meta.cover
-                else None
             ),
             "audio_url": (
                 _url_from_resource(meta.meta_tags.audio)
