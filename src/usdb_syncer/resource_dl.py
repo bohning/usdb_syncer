76b28213fb34faf4cb3c7485b69815fa4cd690f9
Markus Böhning <1497707+bohning@users.noreply.github.com>
2024-04-01 19:30:54 +0200

    Update User-Agent.

--- src/usdb_syncer/resource_dl.py
+++ src/usdb_syncer/resource_dl.py
@@ -22,7 +22,7 @@ from usdb_syncer.utils import url_from_resource
 IMAGE_DOWNLOAD_HEADERS = {
     "User-Agent": (
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-        "(KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"
+        "(KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
     )
 }
 
