7db0c7ad2676898614217c1deceb6fa23e61c480
Arjan Speiard | Asgard Sings! <arjan.spieard@gmail.com>
2024-09-04 12:40:46 +0200

    Added a way to post comments and a rating onto one song
    It uses a form-url-encoded post request. Usecase for this will be to easily add tags or video's as comments.

---
+++ src/usdb_syncer/gui/comment_dialog.py
@@ -0,0 +1,39 @@
+"""Dialog to post a comment on one or multiple songs."""
+
+from PySide6.QtWidgets import QDialog, QWidget
+
+from usdb_syncer.gui.forms.CommentDialog import Ui_Dialog
+from usdb_syncer.usdb_scraper import RequestMethod, get_usdb_page
+from usdb_syncer.usdb_song import UsdbSong
+
+
+class CommentDialog(Ui_Dialog, QDialog):
+    """Dialog to post a comment on one or multiple songs."""
+
+    def __init__(self, parent: QWidget, selected_song: UsdbSong) -> None:
+        super().__init__(parent=parent)
+        self._parent = parent
+        self.setupUi(self)
+        self._selected_song = selected_song
+        self.combobox_rating.addItem("neutral")
+        self.combobox_rating.addItem("positiv")
+        self.combobox_rating.addItem("negativ")
+
+    def accept(self) -> None:
+        payload = {
+            "text": self.text_edit_comment.toPlainText(),
+            "stars": self.combobox_rating.currentText(),
+        }
+
+        get_usdb_page(
+            "index.php",
+            RequestMethod.POST,
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+            params={
+                "link": "detail",
+                "id": str(self._selected_song.song_id),
+                "comment": str(1),
+            },
+            payload=payload,
+        )
+        super().accept()
