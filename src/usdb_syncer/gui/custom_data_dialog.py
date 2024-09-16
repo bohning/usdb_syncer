58c753a8f0433bd64fb4328eb4f14e146232126b
RumovZ <gp5glkw78@relay.firefox.com>
2024-09-16 20:57:21 +0200

    Forbid special characters from custom data keys

--- src/usdb_syncer/gui/custom_data_dialog.py
+++ src/usdb_syncer/gui/custom_data_dialog.py
@@ -6,6 +6,8 @@ from PySide6 import QtWidgets
 
 from usdb_syncer.gui.forms.CustomDataDialog import Ui_Dialog
 
+FORBIDDEN_CHARS = '?"<>|*.:/\\'
+
 
 class CustomDataDialog(Ui_Dialog, QtWidgets.QDialog):
     """Dialog with about info and credits."""
@@ -23,10 +25,17 @@ class CustomDataDialog(Ui_Dialog, QtWidgets.QDialog):
             self.edit_key.setText(key)
 
     def accept(self) -> None:
-        if (key := self.edit_key.text()) and (value := self.edit_value.text()):
-            self._on_accept(key, value)
-            super().accept()
-        else:
+        key = self.edit_key.text().strip()
+        value = self.edit_value.text().strip()
+        if not key or not value:
+            warning = "Both key and value must be supplied!"
             QtWidgets.QMessageBox.warning(
                 self, "Warning", "Both key and value must be supplied!"
             )
+        elif any(c in key for c in FORBIDDEN_CHARS):
+            warning = f"Key must not contain any of these characters: {FORBIDDEN_CHARS}"
+        else:
+            self._on_accept(key, value)
+            super().accept()
+            return
+        QtWidgets.QMessageBox.warning(self, "Warning", warning)
