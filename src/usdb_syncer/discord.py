03ab73be6b20ec631d89e0d3eea7a2cc2dfac276
Markus Böhning <1497707+bohning@users.noreply.github.com>
2025-03-02 17:27:24 +0100

    Avoid multiple notifications for same resource.
    This is reset with a restart of the Syncer and does not avoid multiple clients sending the same message. To avoid this, a shared notifications database would be needed.

--- src/usdb_syncer/discord.py
+++ src/usdb_syncer/discord.py
@@ -11,22 +11,23 @@ CHANNEL = "1231022685513973840"
 HASH = "0MfBIqFH9JFl4zALvqdm-etzIuT6OefxH12YXjCTMw7xyyhUlSL1oJkxjnifQmrZ2tzD"
 WEBHOOK_URL = f"{BASE_URL}/{CHANNEL}/{HASH}"
 
+sent_notifications: set[tuple[SongId, str]] = set()
+
 
 def notify_discord(song_id: SongId, url: str) -> None:
     """Notify unavailable resources on Discord"""
 
+    if (song_id, url) in sent_notifications:
+        return
     webhook = discord.SyncWebhook.from_url(WEBHOOK_URL)
-
-    embed = discord.Embed(
-        description=f"Failed resource: {url}", color=discord.Color.red()
-    )
-
+    embed = discord.Embed(color=discord.Color.red())
     if not (song := UsdbSong.get(song_id)):
         return
-
     embed.set_author(
         name=f"{song_id:d}: {song.artist} - {song.title}",
         url=f"{Usdb.DETAILS_URL}{song_id:d}",
         icon_url=f"{Usdb.COVER_URL}{song_id:d}.jpg",
     )
+    embed.add_field(name="Failed resource:", value=url, inline=False)
     webhook.send(embed=embed)
+    sent_notifications.add((song_id, url))
