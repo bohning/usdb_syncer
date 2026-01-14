bdd7801d073446c41f1a6b0eb82b8a261b472111
randompersona1 <74961116+randompersona1@users.noreply.github.com>
2026-01-14 23:03:35 +0100

    Fix test

--- tests/unit/test_addons.py
+++ tests/unit/test_addons.py
@@ -15,13 +15,7 @@ def func(arg1, arg2):
     hooks.SampleHook.unsubscribe(func)
     return 42
 
-
-def func2(arg1, arg2):
-    pass
-
 hooks.SampleHook.subscribe(func)
-hooks.SampleSingleHook.subscribe(func2)
-hooks.SampleSingleHook.subscribe(func2)
 """
 
 
@@ -29,12 +23,7 @@ class SampleHook(hooks._Hook[[str, str], int]):
     ""
 
 
-class SampleSingleHook(hooks._SingleSubscriberHook[[str, str], int]):
-    ""
-
-
 hooks.SampleHook = SampleHook  # type: ignore[attr-defined]
-hooks.SampleSingleHook = SampleSingleHook  # type: ignore[attr-defined]
 
 
 @pytest.mark.parametrize(
@@ -44,7 +33,6 @@ def test_loading_add_on_package(file: Path) -> None:
     if "test_addon" in sys.modules:
         del sys.modules["test_addon"]
     SampleHook._subscribers.clear()  # type: ignore[misc]
-    SampleSingleHook._subscribers.clear()  # type: ignore[misc]
     with tempfile.TemporaryDirectory() as tempdir:
         utils.AppPaths.addons = Path(tempdir, "addons")
         file = utils.AppPaths.addons / file
@@ -52,7 +40,6 @@ def test_loading_add_on_package(file: Path) -> None:
         file.write_text(ADD_ON, encoding="utf-8")
         addons.load_all()
         assert len(SampleHook._subscribers) == 1  # type: ignore[misc]
-        assert len(SampleSingleHook._subscribers) == 1  # type: ignore[misc]
         ret = list(SampleHook.call("arg1", "arg2"))
         assert ret == [42]
         assert len(SampleHook._subscribers) == 0  # type: ignore[misc]
